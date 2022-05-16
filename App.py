3bf228b57fdeca7d4297f69b6572692845dc0bb7
Bruno Brocanelli <bbrocanelli@gmail.com>
2022-05-16 17:07:19 -0300

    Inserting Data

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Data():
+class Data(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
@@ -20,11 +20,30 @@ class Data():
         self.phone = phone
         
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+
 
 
 if __name__ == "__main__":
