6be09bfc9694a74ac28f9c09ec683a3d4ddc9486
Bruno Brocanelli <bbrocanelli@gmail.com>
2022-05-16 18:01:43 -0300

    Adding Flash messages

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -32,6 +32,8 @@ def insert():
         my_data = Data(name, email, phone)
         db.session.add(my_data)
         db.session.commit()
+
+        flash("Employee inserted Successfully")
 
         return redirect(url_for('index'))
 
